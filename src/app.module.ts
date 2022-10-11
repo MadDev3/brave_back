fb3eedc3ec2803291826774e7d65dc28c1948eba
vadim <vmakeev.dev@gmail.com>
2022-10-11 10:19:09 +0400

    Add user sign

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import {Module} from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { SequelizeModule } from "@nestjs/sequelize";
+import { User } from "./users/users.model";
 import { UsersModule } from './users/users.module';
 
 
@@ -15,10 +16,10 @@ import { UsersModule } from './users/users.module';
             dialect: 'postgres',
             host: process.env.POSTGRES_HOST,
             port: Number(process.env.POSTGRES_PORT),
-            username: process.env.POSTGRES_USERNAME,
+            username: process.env.POSTGRES_USER,
             password: process.env.POSTGRES_PASSWORD,
             database: process.env.POSTGRES_DB,
-            models: [],
+            models: [User],
             autoLoadModels: true
         }),
         UsersModule,
